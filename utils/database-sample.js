d173e0faf33439a1686899b71fd81d906a55684a
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-06-18 03:25:46 -0400

    asdasd

--- utils/database-sample.js
+++ utils/database-sample.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // const { mongouser, mongopass } = process.env;
 
 exports.connectToDB = () => {
-    return mongoose.connect("mongodb://localhost:27017/toplearn", {
+    return mongoose.connect("mongodb+srv://Jason:HL0wqOnk9fuYHQcZ@jasondevconnector-xi2qh.mongodb.net/toplearn?retryWrites=true&w=majority", {
         useCreateIndex: true,
         useNewUrlParser: true,
         useUnifiedTopology: true
